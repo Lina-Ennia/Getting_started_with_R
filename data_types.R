9ec89461bc9c3f5e2a7d0604522933637bc9e73b
Lina Ennia <92579850+Lina-Ennia@users.noreply.github.com>
2022-11-08 19:28:34 +0100

    Create data_types.R

---
+++ data_types.R
@@ -0,0 +1,20 @@
+#create a numeric vector x with 5 components
+x<-c(1,3,2,10,5)
+x
+## [1]  1  3  2 10  5
+#create a logical vector x
+x<-c(TRUE,FALSE,TRUE)
+x
+## [1]  TRUE FALSE  TRUE
+# create a character vector
+x<-c("sds","sd","as")
+x
+## [1] "sds" "sd"  "as"
+class(x)
+## [1] "character"
+# create an integer vector
+x<-c(1L,2L,3L)
+x
+## [1] 1 2 3
+class(x)
+## [1] "integer"
